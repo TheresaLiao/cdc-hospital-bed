2a659f6bc912eaf9bd9ac6852f6a6d257f6465d9
Paul Shih <endlessme@bitbucket.org>
2020-04-27 15:33:45 +0800

    solved the "no-session" error

--- src/main/java/org/itri/view/humanhealth/dao/PersonInfosDaoHibernateImpl.java
+++ src/main/java/org/itri/view/humanhealth/dao/PersonInfosDaoHibernateImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
@@ -19,13 +20,23 @@ public class PersonInfosDaoHibernateImpl {
 	public List<Patient> getPatientList() {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Transaction tx = null;
+		List<Patient> tempPatientList = new ArrayList<Patient>();
 		List<Patient> patientList = new ArrayList<Patient>();
 		try{
 			tx = session.beginTransaction();
 			
 			Criteria criteria = session.createCriteria(Patient.class);
 			criteria.add(Restrictions.eq("isDeleted", false));
-			patientList = criteria.list();
+			tempPatientList = criteria.list();
+			
+			for (Patient p : tempPatientList) {
+				Hibernate.initialize(p.getPatientInfos());
+				Hibernate.initialize(p.getRtHeartRhythmRecords());
+				Hibernate.initialize(p.getRtOximeterRecords());
+				Hibernate.initialize(p.getRtHeartRhythmRecords());
+				Hibernate.initialize(p.getRtTempPadRecords());
+				patientList.add(p);
+			}
 			tx.commit();
 		} 
 		catch (Exception e){
